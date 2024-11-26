a3a72e88b780b0e7ac6c256937009111c3ea453e
Daniel-Thinkpad <ibt.daniel.fo@gmail.com>
2024-11-26 13:41:50 -0600

    Adding code to co-assembly

--- worflow/snakemake.py
+++ worflow/snakemake.py
@@ -7,26 +7,42 @@
 #### Import Python libraries ####
 #################################
 import glob
-
+import yaml
 
 #################################
 #### Define Global Variables ####
 #################################
-SAMPLE,RF=glob_wildcards("../resources/Data/{sample}_{rf}.fastq.gz")
+# Define groups and their respective samples
+GROUPS = {
+    "PrimaryLagoon": ["AP"],
+    "MacrophyteRoots": ["AR1", "AR2", "AR3"],
+    "Algae": ["M1", "M4", "M9"], 
+    "Lemna" : ["M3", "M5", "M8"],
+    "FTW" : ["M6", "M7"],
+    "SecondaryLagoon": ["AS", "JWC1", "JWC2", "JWF1", "JWF2"], 
+    "FTWRoots": ["JR1", "JR2"], 
+    "Sediments" : ["JS1", "JS2"]
+}
 
+# Extract all samples
+SAMPLES = [sample for group in GROUPS.values() for sample in group]
+RF = ["R1", "R2"]
 
 #########################
 #### General Outputs ####
 #########################
 rule all:
     input: 
-        expand("../resources/Outputs/fastqc_rr/{sample}_{rf}.html", sample=SAMPLE, rf=RF), 
-        expand("../resources/Outputs/fastqc_rr/{sample}_{rf}_fastqc.zip", sample=SAMPLE, rf=RF),
-        expand("../resources/Outputs/trimmed_reads/{sample}_R{pe}.fastq.gz", sample=SAMPLE, pe=["1","2"]), 
-        expand("../results/fastqc_tr/{sample}_{rf}.html", sample=SAMPLE, rf=RF),
-        expand("../resources/Outputs/kraken2_rr/reports/{sample}.txt", sample=SAMPLE),
-        expand("../results/bracken/{sample}.txt", sample=SAMPLE),
-        expand( "../resources/Outputs/mergepe/{sample}.fastq.gz", sample=SAMPLE)
+        # FastQC for raw reads
+        expand("../resources/Outputs/fastqc_rr/{sample}_{rf}.html", sample=SAMPLES, rf=RF), 
+        expand("../resources/Outputs/fastqc_rr/{sample}_{rf}_fastqc.zip", sample=SAMPLES, rf=RF),
+        # Trimmed reads
+        expand("../resources/Outputs/trimmed_reads/{sample}_R{pe}.fastq.gz", sample=SAMPLES, pe=["1","2"]), 
+        # FastQC for trimmed reads
+        expand("../results/fastqc_tr/{sample}_R{pe}.html", sample=SAMPLES, pe=["1","2"]),
+        expand("../results/fastqc_tr/{sample}_R{pe}_fastqc.zip", sample=SAMPLES, pe=["1","2"]),
+        # Co-assembly outputs
+        expand("../results/coassembly/{group}.fasta", group=GROUPS.keys())
 
 #################################
 #### Quality Check raw reads ####
@@ -50,7 +66,6 @@ rule fastqc_rr:
     wrapper:
         "v4.5.0/bio/fastqc"
 
-
 ###################################
 #### Quality Control raw reads ####
 ###################################
@@ -59,8 +74,8 @@ rule trimmomatic_rr:
         r1="../resources/Data/{sample}_R1.fastq.gz",
         r2="../resources/Data/{sample}_R2.fastq.gz"
     output:
-        r1="../resources/Outputs/trimmed_reads/{sample}_R1.fastq.gz", 
-        r2="../resources/Outputs/trimmed_reads/{sample}_R2.fastq.gz", 
+        r1_trimmed="../resources/Outputs/trimmed_reads/{sample}_R1.fastq.gz", 
+        r2_trimmed="../resources/Outputs/trimmed_reads/{sample}_R2.fastq.gz", 
         r1_unpaired="../resources/Outputs/trimmomatic/{sample}_R1.unpaired.fastq.gz", 
         r2_unpaired="../resources/Outputs/trimmomatic/{sample}_R2.unpaired.fastq.gz"
     log:
@@ -70,9 +85,8 @@ rule trimmomatic_rr:
     params:
         trimmer=["TRAILING:15", 
                  "LEADING:15",
-                 #"SLIDINGWINDOW:4:20",
                  "ILLUMINACLIP:../resources/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads",
-                 "MINLEN:50"], #Need to add more arguments
+                 "MINLEN:50"],
         compression_level="-5"
     threads: 
         4
@@ -86,10 +100,10 @@ rule trimmomatic_rr:
 #####################################
 rule fastqc_tr:
     input:
-        reads="../resources/Outputs/trimmed_reads/{sample}_{rf}.fastq.gz"
+        reads="../resources/Outputs/trimmed_reads/{sample}_R{pe}.fastq.gz"
     output: 
-        html="../results/fastqc_tr/{sample}_{rf}.html",
-        zip="../results/fastqc_tr/{sample}_{rf}_fastqc.zip"
+        html="../results/fastqc_tr/{sample}_R{pe}.html",
+        zip="../results/fastqc_tr/{sample}_R{pe}_fastqc.zip"
     params:
         extra="--quiet"
     threads:
@@ -97,8 +111,30 @@ rule fastqc_tr:
     resources:
         mem_mb=4000
     log:
-        "../resources/Logs/fastqc_tr/{sample}_{rf}.log"
+        "../resources/Logs/fastqc_tr/{sample}_R{pe}.log"
     conda:
         "../envs/fastqc_env.yaml"
     wrapper:
         "v4.5.0/bio/fastqc"
+
+#####################
+#### Co-Assembly ####
+#####################
+rule coassemble:
+    input:
+        r1=lambda wildcards: ",".join([f"../resources/Outputs/trimmed_reads/{sample}_R1.fastq.gz" for sample in GROUPS[wildcards.group]]),
+        r2=lambda wildcards: ",".join([f"../resources/Outputs/trimmed_reads/{sample}_R2.fastq.gz" for sample in GROUPS[wildcards.group]])
+    output:
+        fasta="../results/coassembly/{group}.fasta"
+    log:
+        "../resources/Logs/coassembly/{group}.log"
+    conda:
+        "../envs/spades_env.yaml"
+    shell:
+        """
+        mkdir -p $(dirname {output.fasta}) $(dirname {log})
+        spades.py --meta -o ../results/coassembly/{wildcards.group} \
+                  -1 {input.r1} -2 {input.r2} \
+                  > {log} 2>&1
+        """
+
